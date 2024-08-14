a8fb8923f5701a07ba0ab4651f3420d87cb66718
Andrecio Bezerra <andreciobezerra@gmail.com>
2024-08-14 20:47:24 -0300

    create todo_factory

---
+++ src/to_do/mod.rs
@@ -0,0 +1,18 @@
+use enums::TaskStatus;
+use models::{done::Done, pending::Pending};
+
+pub mod enums;
+pub mod models;
+// pub mod traits;
+
+pub enum ItemTypes {
+    Pending(Pending),
+    Done(Done),
+}
+
+pub fn to_do_factory(title: &str, status: TaskStatus) -> ItemTypes {
+    match status {
+        TaskStatus::DONE => ItemTypes::Done(Done::new(title)),
+        TaskStatus::PENDING => ItemTypes::Pending(Pending::new(title)),
+    }
+}
