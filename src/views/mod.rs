18a22062ccecae808509ba00d50fa6dc6e24b7bb
Andrecio Bezerra <andreciobezerra@gmail.com>
2024-08-16 20:48:53 -0300

    crerate view

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -2,6 +2,7 @@ use actix_web::web::ServiceConfig;
 use auth::auth_views_factory;
 
 pub mod auth;
+pub mod to_do;
 
 pub fn views_factory(app: &mut ServiceConfig) {
     auth_views_factory(app);
---
+++ src/views/to_do/create.rs
@@ -0,0 +1,18 @@
+use actix_web::HttpRequest;
+use serde_json::{Map, Value};
+
+use crate::{
+    processes::process_input,
+    state::read_file,
+    to_do::{enums::TaskStatus, to_do_factory},
+};
+
+pub async fn create(req: HttpRequest) -> String {
+    let state: Map<String, Value> = read_file("./state.json");
+    let title = req.match_info().get("title").unwrap().to_string();
+    let item = to_do_factory(&title.as_str(), TaskStatus::PENDING);
+
+    process_input(item, crate::processes::Command::CREATE, &mut state);
+
+    return format!("{} create", title);
+}
